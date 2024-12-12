e13050a87074ab2ec548b611e134b8254b89376e
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2024-12-12 22:19:17 +0300

    Add files via upload

---
+++ weaponproplist.lua
@@ -0,0 +1,61 @@
+local WeaponObjects = {
+
+------------------------- REVOLVER -------------------------
+WEAPON_REVOLVER_CATTLEMAN = `w_dis_rev_cattleman01_2`, -- / `p_revolver_cattleman01` / `mp009_p_revolver_cattleman_cut`
+WEAPON_REVOLVER_CATTLEMAN_MEXICAN = "None",
+WEAPON_REVOLVER_NAVY = "None",
+WEAPON_REVOLVER_NAVY_CROSSOVER = "None",
+WEAPON_REVOLVER_DOUBLEACTION = `w_dis_rev_doubleaction01`,
+WEAPON_REVOLVER_DOUBLEACTION_GAMBLER = "None",
+WEAPON_REVOLVER_SCHOFIELD = `w_dis_rev_schofield01`,
+WEAPON_REVOLVER_LEMAT = `w_dis_rev_lemat01`,
+
+------------------------- PISTOL -------------------------
+WEAPON_PISTOL_SEMIAUTO = `p_pistol_semiauto_cs01x` / `w_dis_pis_semiauto01`,
+WEAPON_PISTOL_MAUSER = `w_dis_pis_mauser01`,
+WEAPON_PISTOL_VOLCANIC = `w_dis_pis_volcanic01`,
+WEAPON_PISTOL_M1899 = `w_pistol_m189902`,
+
+------------------------- REPEATER -------------------------
+WEAPON_REPEATER_WINCHESTER = `w_dis_rep_winchester01`,
+WEAPON_REPEATER_HENRY = `w_dis_rep_henry01`,
+WEAPON_REPEATER_EVANS = `w_repeater_evans01`, -- BAD
+WEAPON_REPEATER_CARBINE = `w_dis_rep_carbine01_1`,
+
+------------------------- RIFLE -------------------------
+WEAPON_RIFLE_VARMINT = "None",
+WEAPON_RIFLE_SPRINGFIELD = `w_dis_rif_springfield01`,
+WEAPON_RIFLE_BOLTACTION = `w_dis_rif_boltaction01`,
+
+WEAPON_SNIPERRIFLE_ROLLINGBLOCK = `w_dis_rif_rollingblock01`,
+WEAPON_SNIPERRIFLE_CARCANO = `w_dis_rif_carcano01`,
+WEAPON_RIFLE_ELEPHANT = `w_dis_rif_elephant01`,
+
+------------------------- SHOTGUN -------------------------
+WEAPON_SHOTGUN_DOUBLEBARREL = `w_dis_sho_doublebarrel01`,
+WEAPON_SHOTGUN_PUMP = `w_dis_sho_pumpaction01`,
+WEAPON_SHOTGUN_REPEATING = `w_dis_sho_repeating01`,
+WEAPON_SHOTGUN_SAWEDOFF = `w_dis_sho_sawed01`,
+WEAPON_SHOTGUN_SEMIAUTO = `w_dis_sho_semiauto01`,
+
+------------------------- OTHER ------------------------- 
+WEAPON_BOW = ``, -- `p_bowdisplay01x`
+WEAPON_BOW_IMPROVED = ``, -- `w_bow_improved01_grip1`
+------------------------- KNIFE ------------------------- All knives can be the same model (Except Jawbone, Machete)
+WEAPON_MELEE_KNIFE = `w_melee_knife17`, -- Similiar Model
+WEAPON_MELEE_KNIFE_TRADER = ``,
+WEAPON_MELEE_KNIFE_JAWBONE = `w_melee_knife03`,
+WEAPON_MELEE_MACHETE = `w_melee_machete04`,
+WEAPON_MELEE_HAMMER = ``,
+WEAPON_MELEE_MACHETE_COLLECTOR = ``,
+WEAPON_MELEE_CLEAVER = ``,
+WEAPON_MELEE_HATCHET_HUNTER = ``,
+WEAPON_MELEE_KNIFE_HORROR = ``,
+WEAPON_MELEE_KNIFE_RUSTIC = ``,
+
+------------------------- THROWABLE -------------------------
+WEAPON_THROWN_MOLOTOV = `p_throw_molotov01`, --`p_molotov01x` / `p_molotovempty01x` / `s_cft_molotov01` / s_cft_molotov02
+WEAPON_THROWN_TOMAHAWK = ``, -- Can be `p_axe01x`
+WEAPON_THROWN_THROWING_KNIVES = ``, -- Can be `p_knivesbundle02p`
+
+}
